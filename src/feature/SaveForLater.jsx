076d95873454964e94a235694f1d90988fb4ae3b
Vishwani Patel <vishwani.patel@1rivet.local>
2023-08-16 16:36:27 +0530

    feat: saved/unsaved bookmark icon

--- src/feature/SaveForLater.jsx
+++ src/feature/SaveForLater.jsx
@@ -1,25 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BookmarkIcon } from "@heroicons/react/outline";
 import { BookmarkIcon as SolidBookmarkIcon } from "@heroicons/react/solid";
 import { postUserSavedNewsData } from "../services/SavedNewsDataService";
+import UseBookmarkNewsData from "../hooks/UseBookmarkNewsData";
 const SaveForLater = ({ news }) => {
+  const savedData = UseBookmarkNewsData();
+  const [savedNews, setNewsData] = useState(null);
   const userId = localStorage.getItem("userId");
-
-  /**
-   *
-   */
   const handleSavedNewsData = () => {
     postUserSavedNewsData(news, userId);
   };
+  useEffect(() => {
+    for (const newsItem of savedData) {
+      if (news.id == newsItem.id) {
+        setNewsData({
+          ...news,
+          isSaved: true,
+        });
+        return; // Exit loop after match is found
+      }
 
+      setNewsData({
+        ...news,
+        isSaved: false,
+      });
+    }
+  }, [news, savedData]);
   return (
     <>
-      <div className="flex  items-center justify-center  w-10 h-10 rounded-full bg-white shadow-lg flex-shrink-0 absolute -top-5 right-5 md:static ">
-        <BookmarkIcon className="block h-4 w-4" onClick={handleSavedNewsData} />
-      </div>
-      <div className="flex  items-center justify-center  w-10 h-10 rounded-full bg-white shadow-lg flex-shrink-0 absolute -top-5 right-5 md:static">
-        <SolidBookmarkIcon className=" block h-5 w-5" />
-      </div>
+      {savedNews && (
+        <div className="flex items-center justify-center w-10 h-10 rounded-full bg-white shadow-lg flex-shrink-0 absolute -top-5 right-5 md:static">
+          {savedNews.isSaved ? (
+            <SolidBookmarkIcon className="block h-5 w-5" />
+          ) : (
+            <BookmarkIcon
+              className="block h-4 w-4"
+              onClick={handleSavedNewsData}
+            />
+          )}
+        </div>
+      )}
     </>
   );
 };
--- src/services/NewsDataService.jsx
+++ src/services/NewsDataService.jsx
@@ -2,6 +2,6 @@ import axios from "axios";
 const baseUrl = "https://inshorts.com/api/en/news?category=";
 // to get all news
 export const getAllNewsData = async (category) => {
-  const newsData = `${baseUrl}${category}&max_limit=200&include_card_data=true`;
+  const newsData = `${baseUrl}${category}&max_limit=20&include_card_data=true`;
   return await axios.get(`${newsData}`);
 };
